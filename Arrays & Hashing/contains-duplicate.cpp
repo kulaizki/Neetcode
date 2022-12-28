e28ec99c97362ef9e33715c5cb0307543bd0e668
kulaizki <91787757+kulaizki@users.noreply.github.com>
2022-12-28 16:44:38 +0800

    Create contains-duplicate.cpp

---
+++ Arrays & Hashing/contains-duplicate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for (int x: nums) mp[x]++;
+        for (int x: nums) if (mp[x] > 1) return true;
+            
+        return false;
+    }
+};
