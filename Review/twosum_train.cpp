2b60515a92d331bdd089bfe31cb438686e226a70
kulaizki <kulaizke@gmail.com>
2022-12-30 14:50:26 +0800

    updateTwoSumTrain

--- Review/twosum_train.cpp
+++ Review/twosum_train.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 vector<int> twoSum(vector<int>& numbers, int target) {
-    unordered_map<int, int> hash; // Number, Index       
+    unordered_map<int, int> h; // Number, Index       
     for (int i = 0; i < numbers.size(); i++) {
         int numberToFind = target - numbers[i];
-        if (hash.find(numberToFind) != hash.end()) 
-            return {hash[numberToFind], i};
-        hash[numbers[i]] = i;
+        if (h.find(numberToFind) != h.end()) 
+            return {h[numberToFind], i};
+        h[numbers[i]] = i;
     }
     return {};
 }
