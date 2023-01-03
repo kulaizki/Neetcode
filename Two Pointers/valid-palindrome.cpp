81f9ac387633849090fc1c1871faf4b6a44b10ed
kulaizki <91787757+kulaizki@users.noreply.github.com>
2023-01-03 23:05:03 +0800

    Update valid-palindrome.cpp

--- Two Pointers/valid-palindrome.cpp
+++ Two Pointers/valid-palindrome.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i = 0;
-        int j = s.size() - 1;
+        int i = 0,
+            j = s.size() - 1;
         
         while (i < j) {
             while (!isalnum(s[i]) && i < j) {
