664bbe2afcf5af53d4029bf3068a5040ce58fbea
kulaizki <91787757+kulaizki@users.noreply.github.com>
2023-01-03 23:03:22 +0800

    Update longest-consecutive-sequence.cpp

--- Arrays & Hashing/longest-consecutive-sequence.cpp
+++ Arrays & Hashing/longest-consecutive-sequence.cpp
@@ -3,9 +3,8 @@ public:
     int longestConsecutive(vector<int>& nums) {
         unordered_set<int>s(nums.begin(), nums.end());
         int longest = 0;
-        for(auto &n: s){
-            //if this is the start of the sequence
-            if(!s.count(n - 1)){
+        for (auto &n: s) {
+            if (!s.count(n - 1)) {
                 int length = 1; 
                 while(s.count(n + length))
                     ++length;
